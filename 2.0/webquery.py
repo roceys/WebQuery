8ad99b46c9dcce480f55b906c6d7971770c4072a
khuang6 <khelivol_huang@apl.com>
2018-01-05 19:35:08 +0800

    change 2.0 init file

--- 2.0/webquery.py
+++ 2.0/webquery.py
@@ -18,7 +18,7 @@
 
 from anki.hooks import addHook
 
-__version__ = '1.2.0'
+__version__ = '1.2.1'
 
 
 # USER PLEASE READ
@@ -27,39 +27,7 @@ __version__ = '1.2.0'
 
 def start():
     import WebQuery
-    WebQuery.WebQryAddon(
-        load_on_question=True,
-        image_quality=50,
-        preload=True,
-        provider_urls=[
-            ("Bing", "https://www.bing.com/images/search?q=%s"),
-            ("Wiki", "https://en.wikipedia.org/wiki/?search=%s"),
-
-            # REMOVE BELOW "#" IN FRONT OF A LINE TO ENABLE THE PROVIDER URL
-
-            # ("Renren", "http://www.91dict.com/words?w=%s"),
-            # ("DWDS", "https://www.dwds.de/wb/%s"),
-            # ("Littre", "https://www.littre.org/definition/%s"),
-            # ("vajehyab", "https://www.vajehyab.com/?q=%s"),
-            # ("naver", "http://hanja.naver.com/search?query=%s"),
-            # ("MFCCD", "http://humanum.arts.cuhk.edu.hk/Lexis/lexi-mf/search.php?word=%s"),
-            # ("takuhon", "http://coe21.zinbun.kyoto-u.ac.jp/djvuchar?query=%s"),
-            # ("shufa", "http://shufa.guoxuedashi.com/?sokeyshufa=%s&submit=&kz=70"),
-            # ("hanviet", "http://hanviet.org/hv_timchu_ndv.php?unichar=%s"),
-            # ("Etimo", "https://www.etimolojiturkce.com/kelime/%s"),
-            # ("Nisanyan", "http://www.nisanyansozluk.com/?k=%s"),
-            # ("etybank", "http://www.etymologiebank.nl/zoeken/%s"),
-            # ("swahili", "http://siwaxili.com/%s"),
-            # ("turner",
-            #  "http://dsalsrv02.uchicago.edu/cgi-bin/philologic/search3advanced?dbname=turner&query=%s&matchtype=exact&display=utf8"),
-            # ("schmidt", "http://dsalsrv02.uchicago.edu/cgi-bin/app/schmidt_query.py?qs=%s&searchhws=yes"),
-            # ("chgis", "http://maps.cga.harvard.edu/tgaz/placename?fmt=html&n=%s"),
-            # ("CVH", "http://www.cvh.ac.cn/ppbc/%s"),
-            # ("CFH", "http://www.cfh.ac.cn/Spdb/spsearch.aspx?aname=%s"),
-            # ("FRPS", "http://frps.eflora.cn/frps?id=%s"),
-            # ("PPBC", "http://www.plantphoto.cn/list?keyword=%s")
-        ]
-    )
+    WebQuery.WebQryAddon(version=__version__)
 
 
 addHook("profileLoaded", start)
